eaae69670816b14e388564068f3e6d657f68ae4e
Keks <keks@htmlacademy.ru>
2022-10-19 22:14:54 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,4 @@
+import './create-data.js';
+import './create-similar-cards.js';
+import './form.js';
+
---
+++ 6/js/utils.js
@@ -0,0 +1,41 @@
+//Максимум и минимум включаются. Resourse MDN
+function getRandomNumber(min, max) {
+  if(min >= max || min < 0 || max < 0) {
+    return NaN;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getRandomCoordinate(min, max, number) {
+  if(min >= max || min < 0 || max < 0) {
+    return NaN;
+  }
+
+  min = Math.floor(min);
+  max = Math.floor(max);
+
+  const randomNumber = Math.random() * (max - min + 1) + min; // Максимум и минимум включаются
+  const coordinate = +randomNumber.toFixed(number);
+
+  return coordinate;
+}
+
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+
+// склонение слов
+const inflectWord = (number, words) => {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return words[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+};
+
+
+export { getRandomNumber, getRandomCoordinate, getRandomArrayElement, inflectWord };
+
+
